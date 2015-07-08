839d7b342c1e14422a75afabbaffea3657b925d1
ZhreShold <cheungchih@gmail.com>
2015-07-08 16:46:56 -0700

    Update directory.py

--- directory.py
+++ directory.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import fnmatch
 
 #######################################
 ### create directory if not exist ###
@@ -27,3 +28,22 @@ def mkdir(path):
   else:
       print("At least python 2.7 required")
       raise 
+
+####################################
+### list subdirs in root folder ###
+def list_dir(root):
+  return [x for x in os.listdir(root) if os.path.isdir(x)]
+  
+################################################
+### list files with extension in root folder ###
+def list_files_with_extension(root, ext):
+  return [x for x in os.listdir(root) if os.path.isfile(x) and os.path.splitext(x)[1]==ext]
+
+####################################################
+### list files recursively with pattern to match ###
+# py_files = list_files_recursive('.', '*.py')
+def list_files_recursive(root, pattern):
+  matches = []
+  for root, dirnames, filenames in os.walk(root):
+    for filename in fnmatch.filter(filenames, pattern):
+      matches.append(os.path.join(root, filename))
